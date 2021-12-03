2e0244d965f78838ea376b91306b1dc03fd64716
park4ever <signpyw@naver.com>
2021-12-03 19:17:54 +0900

    first commit

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "1ed1e9cda7b27640edb36941c3c23d54";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
